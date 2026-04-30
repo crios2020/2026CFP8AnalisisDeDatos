c08d81239a1a1e365e403d7a96c08d24d7e850c6
Carlos <lawlercarlospatricio@gmail.com>
2026-04-29 22:11:56 -0300

    Clase20

--- Clase20/testPandas.py
+++ Clase20/testPandas.py
@@ -13,12 +13,22 @@ import pandas as pd
 # PIP: Python Installer Package
 
 
-#df=pd.read_xml('libros.xml')
+df=pd.read_xml('libros.xml')
 #df=pd.read_csv('libros.csv', encoding='utf-8')
-df=pd.read_excel('libros.xlsx')
+#df=pd.read_excel('libros.xlsx')
 
 #Muestra las primeras filas
 #print(df.head)
 
+#Listado total de libros
 #print(df)
-print(df[df['anio_publicacion']>2007])
+
+#Libros del 2007
+#print(df[df['anio_publicacion']>2007])
+
+#Libros publicados entre 2000 y 2005
+#print(df[(df['anio_publicacion'] >= 2000) & (df['anio_publicacion'] <= 2005)])
+print(df[df['anio_publicacion'].between(2000, 2005)])
+
+#Libros de Borges
+print(df[df['autor'].str.contains('Borges', case=False)])
